d8ff301012e5d500de1361222c4ea8d624b3300c
mequam <blue9ja@gmail.com>
2023-07-20 16:20:00 -0400

    finished spacing on the legs! IK

--- scenes/spidorIK/ikSpider.gd
+++ scenes/spidorIK/ikSpider.gd
@@ -5,39 +5,35 @@ extends Node3D
 @export var ik : PackedScene
 
 
+
 #the number of legs and segments in the spidor
 @export var leg_count : int #the count of total legs
 
 
+@export_range(0,1) var leg_offset : float 
+
+
+func add_ik(i,lr):
+	var ik_local = self.ik.instantiate()
+	ik_local.root_bone = StringName(  "Leg " + str(i) + " 0 " + lr ) 
+	ik_local.tip_bone = StringName(  "Leg " + str(i) + " Target " + lr ) 
+	
+	var target = Node3D.new()
+	target.name = "Leg " + str(i) + " target"
+	targets.add_child(target)
+	ik_local.target_node = target.get_path()
+
+	var angle = float(i)/leg_count 
+	angle = remap(angle,0,1,PI/4,2*PI)
+	var offset = leg_offset*PI
+	target.transform.origin.x = cos(angle + offset)*19 * (1 if lr == 'L' else -1)
+	target.transform.origin.z = -sin(angle + offset)*19
+
+	skele.add_child(ik_local)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for i in range(leg_count / 2):
-		
 		#left 
-		var ik_local = self.ik.instantiate()
-		ik_local.root_bone = StringName(  "Leg " + str(i) + " 0 L" ) 
-		ik_local.tip_bone = StringName(  "Leg " + str(i) + " Target L" ) 
-		
-		var target = Node3D.new()
-		target.name = "Leg " + str(i) + " target"
-		targets.add_child(target)
-		ik_local.target_node = target.get_path()
-	
-		skele.add_child(ik_local)
-
+		add_ik(i,"L")
 		#right
-		ik_local = self.ik.instantiate()
-		ik_local.root_bone = StringName(  "Leg " + str(i) + " 0 R" ) 
-		ik_local.tip_bone = StringName(  "Leg " + str(i) + " Target R" ) 
-
-		target = Node3D.new()
-		target.name = "Leg " + str(i) + " target"
-		targets.add_child(target)
-		ik_local.target_node = target.get_path()
-
-		skele.add_child(ik_local)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+		add_ik(i,"R")
