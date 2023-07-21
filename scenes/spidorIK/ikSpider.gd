bee9eb4bfe3f3eda3091a332cf52808e16916169
XGit3000 <sebastian.carrier@gmail.com>
2023-07-20 21:33:15 -0400

    spidor average normal attemps 2 ugh
    ugh

--- scenes/spidorIK/ikSpider.gd
+++ scenes/spidorIK/ikSpider.gd
@@ -5,12 +5,13 @@ class_name Spidor
 
 var originial_transform : Transform3D
 
-@export var skele  : Skeleton3D
+@export var skele  : Skeleton3D # 💀
 @export var targets : Node3D 
 @export var raycasts : Node3D
 @export var ik : PackedScene
 
-
+@export var left_leg_positions : PackedVector3Array
+@export var right_leg_positions : PackedVector3Array
 
 #the number of legs and segments in the spidor
 @export var leg_count : int #the count of total legs
@@ -18,23 +19,48 @@ var originial_transform : Transform3D
 
 @export_range(0,1) var leg_offset : float 
 
+@export var debug_mouse_sensitivity : float = 1.0
 
 var average_normal : Vector3 = Vector3.UP
 func align_to_average_norm()->void:
-	global_transform = originial_transform.looking_at(global_position + average_normal)
+	#global_transform = originial_transform.looking_at(global_position + average_normal)
+	var llp = left_leg_positions
+	var rlp = right_leg_positions
+	var norms = [
+		get_normal_of_points(llp[3],rlp[0],llp[0]),
+		get_normal_of_points(llp[3],rlp[1],llp[0]),
+		get_normal_of_points(llp[3],rlp[2],llp[0]),
+		get_normal_of_points(llp[3],rlp[3],llp[0]),
+		get_normal_of_points(rlp[3],llp[0],rlp[0]),
+		get_normal_of_points(rlp[3],llp[1],rlp[0]),
+		get_normal_of_points(rlp[3],llp[2],rlp[0]),
+		get_normal_of_points(rlp[3],llp[3],rlp[0]),
+	]
+#	print(get_normal_of_points(llp[3],rlp[2],llp[0]))
+#	print(norms)
+#	print(norms.reduce(func(accume, thing): return accume + thing, Vector3.ZERO).normalized())
+	var cooler_norm = norms.reduce(func(accume, thing): return accume + thing, Vector3.ZERO).normalized()
+#	global_transform = originial_transform.looking_at(global_position + cooler_norm)
+	$TestPlane.global_transform.basis = originial_transform.basis.looking_at(cooler_norm)
+	pass
 
+func get_normal_of_points(first : Vector3, second : Vector3, third : Vector3):
+	return (first-second).cross(third-second).normalized()
+	pass
 
 func add_ik(i,lr):
 	var ik_local = self.ik.instantiate()
 	ik_local.spidor = self
 	ik_local.root_bone = StringName(  "Leg " + str(i) + " 0 " + lr ) 
 	ik_local.tip_bone = StringName(  "Leg " + str(i) + " Target " + lr ) 
+	ik_local.leg_index = i
+	ik_local.left_leg = (lr == "L")
 	
 	var target = Node3D.new()
 	var sphear = CSGSphere3D.new()
 	target.add_child(sphear)
 
-	var raycast =RayCast3D.new()
+	var raycast = RayCast3D.new()
 
 	target.name = "Leg " + str(i) + " target"
 	targets.add_child(target)
@@ -63,8 +89,18 @@ func add_ik(i,lr):
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	originial_transform = self.global_transform
+	left_leg_positions.resize(leg_count/2)
+	right_leg_positions.resize(leg_count/2)
 	for i in range(leg_count / 2):
 		#left 
 		add_ik(i,"L")
+		
 		#right
 		add_ik(i,"R")
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+			global_position += debug_mouse_sensitivity*(Vector3(event.relative.x,event.relative.y, 0.0) if Input.is_key_pressed(KEY_SHIFT) else Vector3(event.relative.x,0.0,event.relative.y))
+		elif Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+			global_position += debug_mouse_sensitivity*(Vector3(event.relative.x,0.0,event.relative.y) if Input.is_key_pressed(KEY_SHIFT) else Vector3(event.relative.x,event.relative.y,0.0))
--- scenes/spidorIK/spidorIK.gd
+++ scenes/spidorIK/spidorIK.gd
@@ -3,6 +3,8 @@ extends SkeletonIK3D
 var spidor : Spidor
 var raycast : RayCast3D 
 var threshold : float  = 600
+var leg_index : int = 0
+var left_leg : bool = false
 
 var ik_target : Node3D
 
@@ -13,8 +15,9 @@ func update_position()->void:
 		spidor.average_normal  = (spidor.average_normal*spidor.leg_count - prev_normal + ray_normal)/spidor.leg_count
 		spidor.align_to_average_norm()
 		ik_target.global_position = raycast.get_collision_point()
-		prev_normal = ray_normal 
-
+		prev_normal = ray_normal
+		if left_leg: spidor.left_leg_positions[leg_index] = raycast.get_collision_point()
+		else: spidor.right_leg_positions[leg_index] = raycast.get_collision_point()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
